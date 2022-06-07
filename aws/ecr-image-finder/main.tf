38779404650571234b4224709833f18cd9f7453a
Marcelo Correia Pinheiro <salizzar@gmail.com>
2022-06-06 21:35:46 -0300

    Update ECR Image Finder module to receive AWS region as argument

--- aws/ecr-image-finder/main.tf
+++ aws/ecr-image-finder/main.tf
@@ -1,6 +1,7 @@
 variable "args" {
   type = object({
     aws_profile    = string
+    aws_region     = string
     ecr_repository = string
     ecr_image_id   = string
   })
@@ -11,6 +12,7 @@ data "external" "finder" {
 
   query = {
     aws_profile    = var.args.aws_profile != null ? var.args.aws_profile : ""
+    aws_region     = var.args.aws_region != null ? var.args.aws_region : ""
     ecr_repository = var.args.ecr_repository
     ecr_image_id   = var.args.ecr_image_id
   }
